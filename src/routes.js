ff6f602565d57ac8c50c19db0a4e9662416a1de7
JeffeVargas <jeffersonvargas885@gmail.com>
2021-05-05 12:02:23 -0300

    UP

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
 const path = require('path')
 const bodyParser = require('body-parser');
 
@@ -21,7 +20,6 @@ app.get('/', (req, res, next) => {
     })
 })
 
-app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
